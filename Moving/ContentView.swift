64fbe246ab84c0e81022cabdd8827edf154616e1
alex-dearden <alex.dearden@clearscore.com>
2020-06-24 08:57:06 +0100

    Initial Commit

---
+++ Moving/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moving
+//
+//  Created by Alex Dearden on 24/06/2020.
+//  Copyright © 2020 Alex Dearden. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
